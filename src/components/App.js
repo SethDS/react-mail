aa1fe31e0ebf7bc231acbeb52e4fd911b9418803
Seth Sutherland <cokolatepal@gmail.com>
2017-03-08 16:02:17 -0700

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
 import React from "react";
-
+import {Link} from 'react-router';
 import Header from "./Header";
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<Header />
+				<img
+					alt="React Logo"
+					src="./assets/react-logo.svg"
+				/>
+				<Header>
+					<Link to="/" p><h1 class="link">rMail</h1></Link>
+					<Link to="inbox"><span>Inbox</span></Link>
+					<Link to="draft"><span>Draft</span></Link>
+					<Link to="contacts"><span>Contacts</span></Link>
+				</Header>
+				{this.props.children}
 			</div>
 		);
 	}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 import {render} from 'react-dom';
+import {Router} from 'react-router';
+import {Route} from 'react-router';
+import {browserHistory} from 'react-router';
 
+import App from './components/App';
+import Inbox from './components/Inbox';
+import Message from './components/Message';
+import Draft from './components/Draft';
+import Contacts from './components/Contact';
+
+
+{/*document.addEventListener( "DOMContentLoaded", () => {*/}
+    {/*const reactNode = document.getElementById("react-node");*/}
+
+    {/*if(reactNode){*/}
+        {/*ReactDOM.render(*/}
+            {/*<Router history={browserHistory}>*/}
+                {/*<Route path ="/" component={App}>*/}
+                    {/*<Route path="draft" component={Draft}/>*/}
+                    {/*<Route path="contacts" component={Contacts}/>*/}
+                    {/*<Route path="inbox" component={Inbox}>*/}
+                        {/*<Route path="/inbox/:messageId" component={Message} />*/}
+                    {/*</Route>*/}
+//                 </Route>
+//             </Router>
+//             , reactNode )
+//     }
+// } );
 render(
-  <h1>Webpack is running! Edit index.js to continue...</h1>,
+
+    <Router history={browserHistory}>
+        <Route path ="/" component={App}>
+            <Route path="draft" component={Draft}/>
+            <Route path="contacts" component={Contacts}/>
+            <Route path="inbox" component={Inbox}>
+                <Route path="/inbox/:messageId" component={Message} />
+            </Route>
+        </Route>
+    </Router>
+    ,
   document.getElementById('root')
-)
+);
